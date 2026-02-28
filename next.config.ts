2d3348cdffb1c579d4a23fc58f3579d4f05a2874
ziadn8n-prog <ziadn8n@gmail.com>
2026-02-28 20:48:34 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  }
+};
 
 export default nextConfig;
